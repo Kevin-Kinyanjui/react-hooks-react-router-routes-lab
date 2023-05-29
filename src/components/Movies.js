9fc7263d9f071c07fee4751a4f3fe9ff42c5af62
Kevin-Kinyanjui <kevin.kinyanjui@student.moringaschool.com>
2023-05-29 21:12:12 +0300

    completed assignment

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,23 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {movies.map((movie) => (
+        <div key={movie.title}>
+          <h3>Title: {movie.title}</h3>
+          <h4>Time: {movie.time}</h4>
+          <ul>
+            Genres:
+            {movie.genres.map((genre) => (
+              <li key={genre}>{genre}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Movies;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,26 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div className="navbar">
+      <nav>
+        <ul>
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/Movies">Movies</NavLink>
+          </li>
+          <li>
+            <NavLink to="/Directors">Directors</NavLink>
+          </li>
+          <li>
+            <NavLink to="/Actors">Actors</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 }
 
 export default NavBar;
